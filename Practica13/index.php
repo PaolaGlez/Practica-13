14c6472022526d05e880b3eb297ca1ed0562979c
PaolaGlez <30761163+PaolaGlez@users.noreply.github.com>
2017-10-12 22:42:50 -0500

    Add files via upload

---
+++ Practica13/index.php
@@ -0,0 +1,21 @@
+<! DOCTYPE html>
+< html >
+< body >
+
+< form  action = " Procesar.php "  method = " post "  name = " DatosPersonales " >
+  Nombre: < br >
+  < input  type = " text "  nombre = " NombreAlumno "  value = " "  placeholder = " Introduce tu Nombre " >
+  < Br >
+  Apellidos: < br >
+  < input  type = " text "  name = " ApellidosAlumno "  value = " "  placeholder = " Introduce tu Apellido " >
+  < Br >
+  Edad: < br >
+  < input  type = " text "  name = " EdadAlumno "  value = " "  placeholder = " Introduce tu Edad " >
+  < Br > < br >
+  < input  type = " submit "  value = " Guardar " >
+</ form >
+
+< p > Mariana Paola Gonzalez Soria </ p >
+
+</ body >
+</ html >
---
+++ Practica13/procesar.php
@@ -0,0 +1,7 @@
+<? php
+$ Nombre = $ _POST [ ' NombreAlumno ' ];
+$ Apellidos = $ _POST [ ' ApellidosAlumno ' ];
+$ Edad = $ _POST [ ' EdadAlumno ' ];
+echo  ' Tu Nombre es: ' . $ Nombre . '  ' . $ Apellidos . ' <br> ' ;
+echo  ' Tu Edad: ' . $ Edad ;
+? >
